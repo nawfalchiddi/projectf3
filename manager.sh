78824bdf32a2683e3a378a6d992f0731bfaf6c57
nawfalchidd <nawfalchiddi@icloud.com>
2026-04-17 10:19:50 -0700

    commit

---
+++ manager.sh
@@ -0,0 +1,24 @@
+while true; do
+    echo "-----"
+    echo "1. Add Student"
+    echo "2. Show Students"
+    echo "3. Search Student"
+    echo "4. Exit"
+    echo "-----"
+    read -p "Select an option: " opt
+
+    if [ "$opt" == "1" ]; then
+        read -p "Enter name: " name
+        read -p "Enter age: " age
+        if [ "$age" -lt 18 ]; then
+            echo "Too young, not allowed."
+        elif [ "$age" -gt 60 ]; then
+            echo "Too old, not allowed."
+        else
+            echo "$name - Age: $age" >> "$students.txt"
+            echo "Student added."
+        fi
+    elif [ "$opt" == "2" ]; then
+        echo "--- Student List ---"
+	 cat "$students.txt
+#zakaria 	 
